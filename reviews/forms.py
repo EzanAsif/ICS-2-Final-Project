f189c5ba1c24e7b9c5ef2bae23e61f44797f1a07
EzanAsif <ezanasif1972@gmail.com>
2021-02-25 03:38:48 +0500

    Fixed Alot of styling

--- reviews/forms.py
+++ reviews/forms.py
@@ -4,4 +4,4 @@ from . import models
 class Createreview(forms.ModelForm):
     class Meta:
         model = models.review
-        fields = ['title', 'body', 'slug', 'thumb']
+        fields = ['title', 'body', 'slug', 'thumb', 'bookAuth']
